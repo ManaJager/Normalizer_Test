a6dfc8abf5858c9ebbeccdb23b9a19d48c2e6f93
Manaze <manaze@yandex.ru>
2021-05-19 22:51:13 +0300

    Добавил комменты

--- src/main/java/normalizer/test.java
+++ src/main/java/normalizer/test.java
@@ -5,27 +5,31 @@ import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 
 public class test {
-    static final int SAMPLES = 1000000;
+    static final int SAMPLES = 10000000;
     static final double[] scoreScale = {0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 
-    static final double rawRiskScore = 0.93;
+    static final double rawRiskScore = 0.79;
 
     public static void main(String[] args) {
+        //Массив для сэмплов rawRickScore
         double[] xVals = new double[SAMPLES];
 
+        //Наполнение массива сэмплов rawRickScore
         for (int i = 0; i < SAMPLES; i++) {
             xVals[i] = Math.random();
+            //System.out.println(xVals[i]);
         }
 
         int riskScore = interpolate(percentilesCalc(xVals), rawRiskScore);
-        System.out.println(riskScore);
+        System.out.println("Normalized riskScore for " + SAMPLES + " random rawRiskScore samples = " + riskScore);
     }
 
+    //Функция расчета перцентилей по сэмплам
     public static double[] percentilesCalc(double[] rawScores) {
         double[] percentiles = new double[11];
         Percentile p = new Percentile().withEstimationType(Percentile.EstimationType.R_7);
         p.setData(rawScores);
-        percentiles[0] = p.evaluate(0.000000000001); //на 0 вылетает ошибка, так что можно бесконечно малое значение вместо этого брать
+        percentiles[0] = p.evaluate(0.00000000001); //на 0 вылетает ошибка, так что можно бесконечно малое значение вместо этого брать
         percentiles[1] = p.evaluate(50);
         percentiles[2] = p.evaluate(70);
         percentiles[3] = p.evaluate(80);
@@ -40,6 +44,7 @@ public class test {
         return percentiles;
     }
 
+    //Функция интерполяции по перцентилям
     public static int interpolate(double[] percentiles, double rawRiskScore) {
         LinearInterpolator linearInterpolator = new LinearInterpolator();
         PolynomialSplineFunction function = linearInterpolator.interpolate(percentiles, scoreScale);
